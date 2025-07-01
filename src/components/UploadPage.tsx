f4538dfa7ae486acdf2fbfc777ec4050e048fe24
Evelyn Jiang <115804711+eve1ynjiang@users.noreply.github.com>
2025-07-01 16:00:53 -0700

    sigh

--- src/components/UploadPage.tsx
+++ src/components/UploadPage.tsx
@@ -1,6 +1,6 @@
 import { useState, useRef } from 'react';
 import '@aws-amplify/ui-react/styles.css';
-import { awsConfig } from '../config';
+//import { awsConfig } from '../config';
 
 interface UploadPageProps {
   onComplete: () => void;
